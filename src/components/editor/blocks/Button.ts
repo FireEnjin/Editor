3fb10c6f10a8be935683ff2703afbb589f85357a
Popcorn245 <bobby@madnesslabs.net>
2022-05-20 01:49:53 -0500

    feat(Button): Add new expand controls to button

--- src/components/editor/blocks/Button.ts
+++ src/components/editor/blocks/Button.ts
@@ -14,6 +14,7 @@ export default class Button {
     "medium",
     "light",
   ];
+  expandOptions = ["block", "full", undefined];
   settings = [
     {
       name: "shape",
@@ -92,6 +93,25 @@ export default class Button {
           buttonEl.color = this.nextArrayItem(this.colorOptions, currentColor);
         } catch (err) {
           console.log("Error setting button alignment!");
+        }
+      },
+    },
+    {
+      name: "expand",
+      innerHTML: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="ionicon" viewBox="0 0 512 512"><title>Expand</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M432 320v112H320M421.8 421.77L304 304M80 192V80h112M90.2 90.23L208 208M320 80h112v112M421.77 90.2L304 208M192 432H80V320M90.23 421.8L208 304"/></svg>`,
+      value: undefined,
+      onClick: () => {
+        try {
+          const buttonEl = this.api.blocks
+            .getBlockByIndex(this.api.blocks.getCurrentBlockIndex())
+            .holder.querySelector("ion-button");
+          const currentExpand = buttonEl?.expand;
+          buttonEl.expand = this.nextArrayItem(
+            this.expandOptions,
+            currentExpand
+          );
+        } catch (err) {
+          console.log("Error setting button expand!");
         }
       },
     },
